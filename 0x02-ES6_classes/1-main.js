00125643557d30726e579453b8fac3161ebe2735
Atikunaja <najaatiku1@gmail.com>
2024-10-03 12:24:09 +0000

    classes

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,1 @@
+import initializeRooms from './1-make_classrooms.js'; console.log(initializeRooms());
---
+++ 0x02-ES6_classes/7-main.js
@@ -0,0 +1,2 @@
+import Airport from "./7-airport.js"; const airportSF = new Airport('San Francisco Airport', 'SFO'); console.log(airportSF); console.log(airportSF.toString()); 
+
